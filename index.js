4c475ab49a2e2a7674fef830cccae576fb036f78
Ian Price <michael.ian.price@gmail.com>
2017-04-03 11:11:22 -0400

    Step 1 complete

--- index.js
+++ index.js
@@ -8,11 +8,28 @@ const pgp = require('pg-promise')({
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
+app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-  res.render('layout.hbs');
+  res.render('home.hbs');
 });
 
+app.post('/search_query', function(req, res) {
+  var qString = req.body.search_query;
+  // Do some stuff with the query
+  console.log(`Received a search query: ${qString}`);
+
+  // Render the search result
+  res.redirect(`/search?searchTerm=${encodeURIComponent(qString)}`);
+});
+
+app.get('/search', function(req, res) {
+  var qString = req.query.searchTerm;
+  qString = decodeURIComponent(qString);
+  res.render('search.hbs', {
+    qString: qString
+  });
+});
 
 
 
